804307d96e4cf902251e97b57157ac1146918fcf
Darwin Algarin <dalgarins@live.com>
2020-04-12 18:35:18 -0500

    added tabs for pet services

---
+++ app/src/main/java/co/anbora/labs/lostpets/ui/petservice/tabs/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package co.anbora.labs.lostpets.ui.petservice.tabs.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
