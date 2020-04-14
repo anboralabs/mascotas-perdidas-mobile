2f240f1a51f895ebc7604067ea75ce2d5682a38f
Darwin Algarin <dalgarins@live.com>
2020-04-13 22:24:52 -0500

    changed login activity to databinding

--- app/src/main/java/co/anbora/labs/lostpets/ui/login/LoginActivity.kt
+++ app/src/main/java/co/anbora/labs/lostpets/ui/login/LoginActivity.kt
@@ -2,12 +2,20 @@ package co.anbora.labs.lostpets.ui.login
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import co.anbora.labs.lostpets.R
+import co.anbora.labs.lostpets.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.signInButton.setOnClickListener {
+
+        }
     }
 }
